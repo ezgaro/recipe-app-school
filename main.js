93283e704e264c63a4c87a756d2c92363bd79229
ezgaro <ezgaro2005@gmail.com>
2024-02-24 19:25:48 +0200

    Save a version that works so I can add other functionality

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const searchForm = document.querySelector("form");
 const searchResultDiv = document.querySelector(".search-result");
-const container = document.querySelector("container");
+const container = document.querySelector(".container");
 let searchQuery = "";
 const APP_ID = "037cecc6";
 const APP_KEY = "d8a4576acefc09f30030f6e3d55a736e";
@@ -16,31 +16,34 @@ async function fetchAPI() {
   const baseUrl = `https://api.edamam.com/search?q=${searchQuery}&app_id=${APP_ID}&app_key=${APP_KEY}&to=20`;
   const response = await fetch(baseUrl);
   const data = await response.json(); //Here is the data with the recipes and the pictures
-  generateHTML(data.hits); //As we can see on the screenshot provided the hits is the array with the recipes
+  generateHTML(data.hits); //As we can see on the screenshot provided the hits are the array with the recipes
 }
 
 
 //Every time we are looping through this array we are creating an item which is a blueprint for the recipes forms
-//TODO refactor the names of the func
 //TODO Add functionality for ingrediands array of data ideas:smth like other btn to click on to go to other page idk 
 function generateHTML(results) {
-  let generatedHTML = "";
+  //Changing the brand name and search bar when we search smth it becomes smaller
+  container.classList.remove("initial");
+  let formHTML = "";
   results.map((result) => {
     //1.From provided data screenshot we take the data.hits.recipe.image which is the item image
     //2.From privided data screenshot we take the data.hits.recipe.label to see the names of the recipes on each form
     //3.Dynamically changing the outcome of clicking the button "View Recipe"
     //4.Dynamically changing the calories and fix them to 2 decimal after the number
-    generatedHTML += `<div class="item">
-        <img src="${result.recipe.image}" alt="" /> 
-        <div class="flex-container">
-          <h1 class="title">${result.recipe.label}</h1>
-          <a class="view-btn" href="${result.recipe.url}" target = "_blank">View Recipe</a>
-        </div>
-        <p class="item-data">Calories: ${result.recipe.calories.toFixed(2)}</p>
-        <p class="item-data">Diet Label: ${result.recipe.dietLabels.length > 0 ? result.recipe.dietLabels : "No Data Found"}</p>
-        <p class="item-data">Health Label: ${result.recipe.healthLabels}</p>
-      </div>`;
+    formHTML += `
+    <div class="item">
+      <img src="${result.recipe.image}" alt="img">
+      <div class="flex-container">
+        <h1 class="title">${result.recipe.label}</h1>
+        <a class="view-btn" target="_blank" href="${result.recipe.url}">View Recipe</a>
+      </div>
+      <p class="item-data">Calories: ${result.recipe.calories.toFixed(2)}</p>
+      <p class="item-data">Diet label: ${result.recipe.dietLabels.length > 0 ? result.recipe.dietLabels : 'No Data Found'}</p>
+      <p class="item-data">Health labels: ${result.recipe.healthLabels}</p>
+    </div>
+  `
   })
   //Add the forms to the website
-  searchResultDiv.innerHTML = generatedHTML;
+  searchResultDiv.innerHTML = formHTML;
 }
